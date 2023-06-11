e08fdf38163351755f9d76e950262189f1a8e061
vini2328 <vinitayarda7@gmail.com>
2023-06-11 17:02:40 +0530

    updated contant.js

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,4 +1,4 @@
-export const BASE_URL='http://localhost:8000/api/user'
+export const BASE_URL='https://auth-backend-ke0o.onrender.com/api/user'
 export const  SIGNUP_URL=BASE_URL+"/register"
 export const LOGIN_URL=BASE_URL+"/login"
 export const RESET_URL=BASE_URL+"/resetpasswordemail"
